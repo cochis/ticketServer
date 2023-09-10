e81c2fe78c75e1b47837305b0998e78a399a8f4f
Oscar Ramirez <ing.oarrs@gmail.com>
2023-09-09 23:30:23 -0600

    login

--- helpers/actualizar-imagen.js
+++ helpers/actualizar-imagen.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const Usuario = require('../models/usuario')
+const Salon = require('../models/salon')
+const Fiesta = require('../models/fiesta')
 
 
 const borrarImagen = (path) => {
@@ -23,6 +25,34 @@ const actualizarImagen = async (tipo, id, nombreArchivo) => {
       }
       usuario.img = nombreArchivo
       await usuario.save()
+      return true
+      break
+    case 'fiestas':
+      const fiesta = await Fiesta.findById(id)
+      if (!fiesta) {
+        return false
+      }
+      pathViejo = `./uploads/fiestas/${fiesta.img}`
+      if (fiesta.img && fiesta.img !== '') {
+
+        borrarImagen(pathViejo)
+      }
+      fiesta.img = nombreArchivo
+      await fiesta.save()
+      return true
+      break
+    case 'salones':
+      const salon = await Salon.findById(id)
+      if (!salon) {
+        return false
+      }
+      pathViejo = `./uploads/salones/${salon.img}`
+      if (salon.img && salon.img !== '') {
+
+        borrarImagen(pathViejo)
+      }
+      salon.img = nombreArchivo
+      await salon.save()
       return true
       break
 
