af21a7559764d94c4f4e6a799343733769e86525
Oscar Ramirez <ing.oarrs@gmail.com>
2023-09-14 15:57:48 -0600

    1.0.2

--- client/assets/examples/core.component.spec.ts
+++
@@ -1,2 +0,0 @@
-db.usuarios.updateMany({ email: "luffy@luffy.com" }, { $set: { password: "$2a$10$osQQ5XA43FwTOzt3V1WEq.CuXzh84hHK80VIh6L6SzuwgaSqZw19m" } })
-db.usuarios.deleteOne({ email: "premirer@premirer.com" })
