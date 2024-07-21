78f8d122114a9ec9c26786a083b3e119fcdb678f
Oscar Ramirez <ing.oarrs@gmail.com>
2024-07-20 19:20:34 -0600

    sitemaps

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const xmlbuilder = require('xmlbuilder');
 const { dbConnection } = require('./database/config')
 const path = require('path')
 const https = require('https')
@@ -15,6 +16,74 @@ app.use(express.static('public'))
 //lectura y paseo del body
 app.use(express.json())
 // Base de datos
+const routes = [
+  '/core',
+  '/core/about',
+  '/core/contact',
+  '/core/pricing',
+  '/core/mis-fiestas',
+  '/core/info-fiesta',
+  '/core/galeria',
+  '/core/salones',
+  '/core/salones/vista-salones',
+  '/core/salones/editar-salon',
+  '/core/salones/crear-salon',
+  '/core/usuarios',
+  '/core/usuarios/vista-usuarios',
+  '/core/usuarios/editar-usuario',
+  '/core/usuarios/crear-usuario',
+  '/core/fiestas',
+  '/core/fiestas/vista-fiestas',
+  '/core/fiestas/editar-fiesta',
+  '/core/fiestas/crear-fiesta',
+  '/core/agregar-invitado',
+  '/core/boletos',
+  '/core/boletos/vista-boletos',
+  '/core/boletos/editar-boleto',
+  '/core/boletos/crear-boleto',
+  '/core/roles',
+  '/core/roles/vista-roles',
+  '/core/roles/editar-rol',
+  '/core/roles/crear-rol',
+  '/core/grupos',
+  '/core/grupos/vista-grupos',
+  '/core/grupos/editar-grupo',
+  '/core/grupos/crear-grupo',
+  '/core/eventos',
+  '/core/eventos/vista-eventos',
+  '/core/eventos/editar-evento',
+  '/core/eventos/crear-evento',
+  '/core/tipo-cantidad',
+  '/core/tipo-cantidad/vista-tipo-cantidades',
+  '/core/tipo-cantidad/editar-tipo-cantidad',
+  '/core/tipo-cantidad/crear-tipo-cantidad',
+  '/core/status-compra',
+  '/core/status-compra/vista-status-compra',
+  '/core/status-compra/crear-status-compra',
+  '/core/status-compra/editar-status-compra',
+  '/core/check-in',
+  '/core/invitaciones/xv/xv2',
+  '/core/templates/default',
+  '/auth/login',
+  '/auth/register',
+
+  // Add more routes as needed
+];
+app.get('/sitemap.xml', (req, res) => {
+  const root = xmlbuilder.create('urlset', { version: '1.0', encoding: 'UTF-8' });
+  root.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+  routes.forEach(route => {
+    const url = root.ele('url');
+    url.ele('loc', `https://tickets.cochisweb.com${route}`);
+    // You can add more elements like <changefreq> and <priority> here if needed
+  });
+
+  res.header('Content-Type', 'application/xml');
+  res.send(root.end({ pretty: true }));
+});
+
+
 dbConnection()
 // Rutas
 app.use('/api/usuarios', require('./routes/usuarios'))
